1bd16567abd93dcb598990ff89ad3f967fc28b12
Theo LEBOUVIER <tlebouvi@e1r1p8.clusters.42paris.fr>
2022-10-26 23:19:49 +0200

    exec added + correc outfile

---
+++ execute/srcs/utils.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlebouvi <tlebouvi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/25 21:16:26 by tlebouvi          #+#    #+#             */
+/*   Updated: 2022/10/13 17:57:06 by tlebouvi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_pipex.h"
+
+void	freetab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	if (tab)
+		free(tab);
+}
+
+void	closefd(int one, int two, int three)
+{
+	close(one);
+	close(two);
+	if (three != -1)
+		close(three);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	size_t	i;
+	size_t	j;
+	size_t	l;
+	size_t	p;
+
+	l = ft_strlen(s1);
+	p = ft_strlen(s2);
+	i = -1;
+	j = -1;
+	dest = malloc(sizeof(char) * ((l + p + 1)));
+	if (!dest)
+		return (NULL);
+	while (++i < l)
+	{
+		dest[i] = s1[i];
+	}
+	while (++j < p)
+	{
+		dest[i] = s2[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+int	ft_putstr_fd(char const *s, int fd)
+{
+	int	i;
+
+	if (s)
+	{
+		i = 0;
+		while (s[i] != '\0')
+		{
+			ft_putchar_fd(s[i], fd);
+			i++;
+		}
+	}
+	return (1);
+}
---
+++ execute/srcs_bonus/execute.c
@@ -0,0 +1,6 @@
+//< infile wc << stop < infile infile infile
+// heredoc> coucou
+// heredoc> stop
+//  3  5 21 infile
+//  3  5 21 infile
+//  6 10 42 total
